b61b91feeb7c67e314eecac18c5580985577337a
frankcrypto2023 <cp6svaen@hotmail.com>
2024-09-11 09:34:34 +0800

    remove unuse files

--- src/eip4337/baseAccountAPI.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.HttpRpcClient = exports.PaymasterAPI = void 0;
-var sdk_1 = require("@account-abstraction/sdk");
-Object.defineProperty(exports, "PaymasterAPI", { enumerable: true, get: function () { return sdk_1.PaymasterAPI; } });
-Object.defineProperty(exports, "HttpRpcClient", { enumerable: true, get: function () { return sdk_1.HttpRpcClient; } });
