aef3ffaa8c5611516e8b9a0194d608d17c0882f4
frankcrypto2023 <cp6svaen@hotmail.com>
2024-09-18 09:01:14 +0800

    fix typo for reference

--- src/eip4337/qngAccountAPI.ts
+++ src/eip4337/qngAccountAPI.ts
@@ -1,14 +1,32 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-import * as contracts_1 from "@qng/eip4337-contracts";
-import { ethers } from "ethers";
+import { BigNumber, BigNumberish } from "ethers";
+import {
+  QngAccount,
+  QngAccount__factory,
+  QngAccountFactory,
+  QngAccountFactory__factory,
+} from "@qng/eip4337-contracts";
+
+import { arrayify, hexConcat } from "ethers/lib/utils";
+import { Signer } from "@ethersproject/abstract-signer";
 import {
   BaseAccountAPI,
   PaymasterAPI,
   HttpRpcClient,
-  // calcPreVerificationGas,
-} from "./baseAccountAPI";
+} from "@account-abstraction/sdk";
+import { BaseApiParams } from "BaseAccountAPI";
 export { PaymasterAPI, HttpRpcClient, BaseAccountAPI };
+/**
+ * constructor params, added no top of base params:
+ * @param owner the signer object for the account owner
+ * @param factoryAddress address of contract "factory" to deploy new contracts (not needed if account already deployed)
+ * @param index nonce value used when creating multiple accounts for the same owner
+ */
+export interface QngAccountApiParams extends BaseApiParams {
+  owner: Signer;
+  factoryAddress?: string;
+  index?: BigNumberish;
+}
+
 /**
  * An implementation of the BaseAccountAPI using the QngAccount contract.
  * - contract deployer gets "entrypoint", "owner" addresses and "index" nonce
@@ -17,37 +35,43 @@ export { PaymasterAPI, HttpRpcClient, BaseAccountAPI };
  * - execute method is "execFromEntryPoint()"
  */
 export class QngAccountAPI extends BaseAccountAPI {
-  factoryAddress: any;
-  owner: any;
-  index: any;
-  accountContract: any;
-  factory: any;
-  constructor(params: any) {
-    var _a;
-    super(params);
+  factoryAddress?: string;
+  owner: Signer;
+  index: BigNumberish;
+
+  /**
+   * our account contract.
+   * should support the "execFromEntryPoint" and "nonce" methods
+   */
+  accountContract?: QngAccount;
+
+  factory?: QngAccountFactory;
+
+  constructor(params: QngAccountApiParams) {
+    super(params as any);
     this.factoryAddress = params.factoryAddress;
     this.owner = params.owner;
-    this.index = ethers.BigNumber.from(
-      (_a = params.index) !== null && _a !== void 0 ? _a : 0
-    );
+    this.index = BigNumber.from(params.index ?? 0);
   }
-  async _getAccountContract() {
+
+  async _getAccountContract(): Promise<QngAccount> {
     if (this.accountContract == null) {
-      this.accountContract = contracts_1.QngAccount__factory.connect(
+      this.accountContract = QngAccount__factory.connect(
         await this.getAccountAddress(),
         this.provider
       );
     }
     return this.accountContract;
   }
+
   /**
    * return the value to put into the "initCode" field, if the account is not yet deployed.
    * this value holds the "factory" address, followed by this account's information
    */
-  async getAccountInitCode() {
+  async getAccountInitCode(): Promise<string> {
     if (this.factory == null) {
       if (this.factoryAddress != null && this.factoryAddress !== "") {
-        this.factory = contracts_1.QngAccountFactory__factory.connect(
+        this.factory = QngAccountFactory__factory.connect(
           this.factoryAddress,
           this.provider
         );
@@ -55,7 +79,7 @@ export class QngAccountAPI extends BaseAccountAPI {
         throw new Error("no factory to get initCode");
       }
     }
-    return (0, ethers.utils.hexConcat)([
+    return hexConcat([
       this.factory.address,
       this.factory.interface.encodeFunctionData("createAccount", [
         await this.owner.getAddress(),
@@ -63,20 +87,26 @@ export class QngAccountAPI extends BaseAccountAPI {
       ]),
     ]);
   }
-  async getNonce() {
+
+  async getNonce(): Promise<BigNumber> {
     if (await this.checkAccountPhantom()) {
-      return ethers.BigNumber.from(0);
+      return BigNumber.from(0);
     }
     const accountContract = await this._getAccountContract();
     return await accountContract.getNonce();
   }
+
   /**
    * encode a method call from entryPoint to our contract
    * @param target
    * @param value
    * @param data
    */
-  async encodeExecute(target: any, value: any, data: any) {
+  async encodeExecute(
+    target: string,
+    value: BigNumberish,
+    data: string
+  ): Promise<string> {
     const accountContract = await this._getAccountContract();
     return accountContract.interface.encodeFunctionData("execute", [
       target,
@@ -84,8 +114,8 @@ export class QngAccountAPI extends BaseAccountAPI {
       data,
     ]);
   }
-  async signUserOpHash(userOpHash: any) {
-    return await this.owner.signMessage((0, ethers.utils.arrayify)(userOpHash));
+
+  async signUserOpHash(userOpHash: string): Promise<string> {
+    return await this.owner.signMessage(arrayify(userOpHash));
   }
 }
-//# sourceMappingURL=QngAccountAPI.js.map
